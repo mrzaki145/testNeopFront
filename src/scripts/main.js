5da33f4132fadad014a1405a585d5d44ced5d966
Mostafa Reda <moszaki1234@gmail.com>
2023-05-19 12:20:39 +0300

    UI Updates

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,10 +9,10 @@ gsap.registerPlugin(ScrollTrigger);
 const introTL = gsap.timeline();
 const { words } = SplitType.create(".hero__title", { types: "words" });
 
-introTL.from(".hero__shape", {
-  scale: 0,
-  opacity: 0,
-});
+// introTL.from(".hero__shape", {
+//   scale: 0,
+//   opacity: 0,
+// });
 
 introTL.fromTo(
   words,
@@ -26,8 +26,7 @@ introTL.fromTo(
     stagger: 0.1,
     duration: 1,
     ease: "power4.out",
-  },
-  "<50%"
+  }
 );
 
 // --------------------- carousel
@@ -43,29 +42,6 @@ const carouselTL = gsap.timeline({
 });
 carouselTL.fromTo("#carousel", { rotateY: -20 }, { rotateY: -250 });
 
-// --------------------- about-us
-
-const aboutUsTL = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".about-us",
-    start: "top 25%",
-  },
-});
-
-aboutUsTL
-  .from(".about-us__shape", {
-    x: 200,
-    opacity: 0,
-  })
-  .from(
-    ".about-us__text",
-    {
-      x: -200,
-      opacity: 0,
-    },
-    "<"
-  );
-
 // --------------------- marquee
 
 const marqueeTL = gsap.timeline({
@@ -77,3 +53,75 @@ const marqueeTL = gsap.timeline({
 });
 
 marqueeTL.to(".marquee", { duration: 5, xPercent: 80 });
+
+// --------------------- shape
+
+const shape = document.querySelector("#shape");
+const shapeWrapper = shape.parentElement;
+
+const shapeTL = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".about-us",
+    scrub: 2,
+    start: "top 25%",
+    end: "bottom bottom",
+    onLeave() {
+      const { top, height } = offset(shapeWrapper);
+      gsap.set(shapeWrapper, {
+        position: "absolute",
+        top: `${top}px`,
+        bottom: "auto",
+        height: `${height}px`,
+      });
+    },
+    onEnterBack() {
+      gsap.set(shapeWrapper, {
+        position: "fixed",
+        top: 0,
+        bottom: "0",
+        height: "auto",
+      });
+    },
+  },
+});
+
+introTL.from(
+  shape,
+  {
+    scale: 0,
+    opacity: 0,
+  },
+  "<25%"
+);
+
+shapeTL.to(shape, {
+  xPercent: 60,
+});
+
+shapeTL.from(
+  ".about-us__text",
+  {
+    x: 200,
+    opacity: 0,
+  },
+  "<"
+);
+
+gsap.from(".contact-us__shape", {
+  xPercent: -50,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".contact-us",
+    start: "top top",
+  },
+});
+
+function offset(el) {
+  let rect = el.getBoundingClientRect();
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  return {
+    top: rect.top + scrollTop,
+    height: rect.height,
+  };
+}
